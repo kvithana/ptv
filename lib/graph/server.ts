b0555af37a673fc69cab6cfc48d1ca53f175fe90
Kalana Vithana <kalana@propps.com>
2024-05-27 16:54:56 +1000

    feat: add apollo armor

--- lib/graph/server.ts
+++ lib/graph/server.ts
@@ -1,7 +1,11 @@
 import { ApolloServer } from "@apollo/server"
+import { ApolloArmor } from "@escape.tech/graphql-armor"
 import { schema } from "./schema"
+
+const armor = new ApolloArmor()
 
 export const server = new ApolloServer({
   schema,
   introspection: true,
+  ...armor.protect(),
 })
