f227a925086cfa15ce1a0c097da1b75b98cc6f83
Kalana Vithana <kalana@propps.com>
2024-05-27 16:41:12 +1000

    chore: add env example and support hitting the prod graph locally

--- lib/hooks/use-server-query.tsx
+++ lib/hooks/use-server-query.tsx
@@ -1,5 +1,6 @@
 import { TypedDocumentNode } from "@graphql-typed-document-node/core"
-import { graphql, print } from "graphql"
+import { GraphQLError, graphql, print } from "graphql"
+import { client } from "../graph/client"
 import { createContext } from "../graph/context"
 import { schema } from "../graph/schema"
 
@@ -8,6 +9,28 @@ export async function useServerQuery<T, U>(
   variables?: U | (() => U)
 ) {
   const v = variables instanceof Function ? variables() : variables
+
+  if (
+    process.env.NODE_ENV === "development" &&
+    process.env.NEXT_PUBLIC_DEPLOYMENT_BASEPATH
+  ) {
+    // developers can hit the production backend from their local development
+    try {
+      const data = await client.request<T>(query, v as any)
+
+      return {
+        data: data as T,
+        error: null,
+      }
+    } catch (err) {
+      const reqRes = JSON.parse(JSON.stringify(err))
+
+      return {
+        data: null,
+        error: reqRes.response.errors as GraphQLError[],
+      }
+    }
+  }
 
   const response = await graphql({
     schema,
