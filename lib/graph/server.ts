c3368df1feca7c056c40bdae3eb0444e6048e032
Kalana Vithana <kalana@propps.com>
2024-05-27 17:11:22 +1000

    feat: set max query depth to 10

--- lib/graph/server.ts
+++ lib/graph/server.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from "@apollo/server"
 import { ApolloArmor } from "@escape.tech/graphql-armor"
 import { schema } from "./schema"
 
-const armor = new ApolloArmor()
+const armor = new ApolloArmor({
+  maxDepth: {
+    n: 10,
+  },
+})
 
 export const server = new ApolloServer({
   schema,
